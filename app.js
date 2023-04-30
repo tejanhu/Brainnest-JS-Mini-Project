8f36bce6be1c7a1ac1b21fe234dbb9527f318330
Hussein Tejan <tejanahmedhu@gmail.com>
2023-04-30 12:30:48 +0100

    Updated variable names and randomising

--- app.js
+++ app.js
@@ -1,82 +1,77 @@
-var pc_selection = computerPlay();
+var computer_selection = computerPlay();
 var player_selection = " ";
 var player_score = 0;
-var pc_score = 0;
+var computer_score = 0;
+var tools = ["rock", "paper", "scissors"];
 
 function computerPlay(){
-    pc_choice = Math.random();
-    if (pc_choice < 0.5) {
-        pc_choice = "Rock";
-    } else if(pc_choice <= 1.5) {
-        pc_choice = "Paper";
-    } else {
-        pc_choice = "Scissors";
-    }
-    return pc_selection = pc_choice;
+    randomise_tools = tools[Math.floor(Math.random()*(tools.length))];
+    console.log("The Computer chose to play: " + randomise_tools);
+    return computer_selection = randomise_tools;
 }
 
 function playerPlay(){
    return player_selection = prompt("What would you like to play with?");  
 }
 
-function playRound(player_selection, pc_selection){
+function playRound(player_selection, computer_selection){
     var msg = "";
 
-    if(pc_selection === ("Paper") && player_selection.toLowerCase() === ("Rock").toLowerCase() && player_selection.toUpperCase() === ("Rock").toUpperCase()){
+    if(computer_selection === ("Paper") && player_selection.toLowerCase() === ("Rock").toLowerCase() && player_selection.toUpperCase() === ("Rock").toUpperCase()){
         console.log("You chose: " + player_selection);
-        console.log("The PC chose: " + pc_selection);
-        pc_score+=10;
+        console.log("The PC chose: " + computer_selection);
+        computer_score+=10;
         msg = "You lost this round! Paper beats Rock!";
     }
-    else if(pc_selection === ("Rock") && player_selection.toLowerCase() === ("Paper").toLowerCase() && player_selection.toUpperCase() === ("Paper").toUpperCase()){
+    else if(computer_selection === ("Rock") && player_selection.toLowerCase() === ("Paper").toLowerCase() && player_selection.toUpperCase() === ("Paper").toUpperCase()){
         console.log("You chose: " + player_selection);
-        console.log("The PC chose: " + pc_selection);
+        console.log("The PC chose: " + computer_selection);
         player_score+=10;
         msg = "You won this round! Paper beats Rock!";
     }
-    else if(pc_selection === ("Scissors") && player_selection.toLowerCase() === ("Paper").toLowerCase() && player_selection.toUpperCase() === ("Paper").toUpperCase()){
+    else if(computer_selection === ("Scissors") && player_selection.toLowerCase() === ("Paper").toLowerCase() && player_selection.toUpperCase() === ("Paper").toUpperCase()){
         console.log("You chose: " + player_selection);
-        console.log("The PC chose: " + pc_selection);
-        pc_score+=10;
+        console.log("The PC chose: " + computer_selection);
+        computer_score+=10;
         msg = "You lost this round! Scissors beats Paper!";
     }
-    else if(pc_selection === ("Paper") && player_selection.toLowerCase() === ("Scissors").toLowerCase() && player_selection.toUpperCase() === ("Scissors").toUpperCase()){
+    else if(computer_selection === ("Paper") && player_selection.toLowerCase() === ("Scissors").toLowerCase() && player_selection.toUpperCase() === ("Scissors").toUpperCase()){
         console.log("You chose: " + player_selection);
-        console.log("The PC chose: " + pc_selection);
+        console.log("The PC chose: " + computer_selection);
         player_score+=10;
         msg = "You won this round! Scissors beats Paper!";
     }
-    else if(pc_selection === ("Rock") && player_selection.toLowerCase() === ("Scissors").toLowerCase() && player_selection.toUpperCase() === ("Scissors").toUpperCase()){
+    else if(computer_selection === ("Rock") && player_selection.toLowerCase() === ("Scissors").toLowerCase() && player_selection.toUpperCase() === ("Scissors").toUpperCase()){
         console.log("You chose: " + player_selection);
-        console.log("The PC chose: " + pc_selection);
-        pc_score+=10;
+        console.log("The PC chose: " + computer_selection);
+        computer_score+=10;
         msg = "You lost this round! Rock beats Scissors!";
     }
-    else if(pc_selection === ("Scissors") && player_selection.toLowerCase() === ("Rock").toLowerCase() && player_selection.toUpperCase() === ("Rock").toUpperCase()){
+    else if(computer_selection === ("Scissors") && player_selection.toLowerCase() === ("Rock").toLowerCase() && player_selection.toUpperCase() === ("Rock").toUpperCase()){
         console.log("You chose: " + player_selection);
-        console.log("The PC chose: " + pc_selection);
+        console.log("The PC chose: " + computer_selection);
         player_score+=10;
         msg = "You won this round! Rock beats Scissors!";
     }
-    else if(pc_selection === ("Rock") && player_selection.toLowerCase() === ("Rock").toLowerCase() && player_selection.toUpperCase() === ("Rock").toUpperCase()){
+    else if(computer_selection === ("Rock") && player_selection.toLowerCase() === ("Rock").toLowerCase() && player_selection.toUpperCase() === ("Rock").toUpperCase()){
         console.log("You chose: " + player_selection);
-        console.log("The PC chose: " + pc_selection);
+        console.log("The PC chose: " + computer_selection);
         player_score+=10;
-        pc_score+=10;
+        computer_score+=10;
         msg = "It's a tie this round! You both have a Rock!";
     }
-    else if(pc_selection === ("Scissors") && player_selection.toLowerCase() === ("Scissors").toLowerCase() && player_selection.toUpperCase() === ("Scissors").toUpperCase()){
+    else if(computer_selection === ("Scissors") && player_selection.toLowerCase() === ("Scissors").toLowerCase() && player_selection.toUpperCase() === ("Scissors").toUpperCase()){
         console.log("You chose: " + player_selection);
-        console.log("The PC chose: " + pc_selection);
+        console.log("The PC chose: " + computer_selection);
         player_score+=10;
-        pc_score+=10;
+        computer_score+=10;
         msg = "It's a tie this round! You both have Scissors!";
     }
-    else if(pc_selection === ("Paper") && player_selection.toLowerCase() === ("Paper").toLowerCase() && player_selection.toUpperCase() === ("Paper").toUpperCase()){
+    else if(computer_selection === ("Paper") && player_selection.toLowerCase() === ("Paper").toLowerCase() && player_selection.toUpperCase() === ("Paper").toUpperCase()){
         console.log("You chose: " + player_selection);
-        console.log("The PC chose: " + pc_selection);
+        console.log("The PC chose: " + computer_selection);
         player_score+=10;
-        pc_score+=10;
+        computer_score+=10;
         msg = "It's a tie this round! You both have Paper!";
     }
     return msg;
@@ -88,22 +83,22 @@ function game(){
        console.log("Round "+ i + ": ");
       
        if(i == 5){
-        if(pc_score > player_score){
+        if(computer_score > player_score){
             console.log("The winner is The PC!");
         }
-        else if(player_score > pc_score){
+        else if(player_score > computer_score){
             console.log("You're the winner!");
         }
-        else if(player_score == pc_score){
+        else if(player_score == computer_score){
             console.log("You both are winners!")
         }
-        console.log("PC SCORE: " + pc_score) 
+        console.log("PC SCORE: " + computer_score) 
         console.log("PLAYER SCORE: " + player_score) 
        }
        else{
         playerPlay();
         computerPlay();
-        console.log(playRound(player_selection, pc_selection));
+        console.log(playRound(player_selection, computer_selection));
        }
     }
 }
